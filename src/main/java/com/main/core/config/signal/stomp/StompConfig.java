a2e0bfd7a0d4107d177690ec051c6a039a4de6e2
system <system>
2017-06-08 12:31:28 +0800

    提交我的项目

--- src/main/java/com/main/core/config/signal/stomp/StompConfig.java
+++ src/main/java/com/main/core/config/signal/stomp/StompConfig.java
@@ -10,14 +10,18 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
  * Created by Administrator on 2017/6/3.
  */
 @Configuration
-//@EnableWebSocketMessageBroker
+@EnableWebSocketMessageBroker
 public class StompConfig extends AbstractWebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config){
         //通过topic和user前缀可以向客户端发送消息(基于内存)
         //config.enableSimpleBroker("/topic","/queue");
         //通过代理中继向客户端发送消息
-        config.enableStompBrokerRelay("/topic","/queue");
+        config.enableStompBrokerRelay("/topic","/queue")
+        .setRelayHost("localhost")
+        .setRelayPort(61613)
+        .setClientLogin("guest")
+        .setClientPasscode("guest");
         //客户端向服务器发送消息需要加上app前缀
         config.setApplicationDestinationPrefixes("/app");
     }
