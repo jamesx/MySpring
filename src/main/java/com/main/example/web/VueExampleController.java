78c688bd4e11497c4b8857fe62eb97049141edef
system <system>
2017-06-11 20:57:33 +0800

    增加rabbit相关配置

---
+++ src/main/java/com/main/example/web/VueExampleController.java
@@ -0,0 +1,17 @@
+package com.main.example.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by xsooy-pc on 2017/6/10.
+ *
+ */
+@Controller
+@RequestMapping("vue")
+public class VueExampleController {
+    @RequestMapping("index")
+    public String index(){
+        return "/web/index";
+    }
+}
