59ad6af07bb97d88f28372e13b4a13f183dc3e24
system <system>
2017-07-10 08:51:26 +0800

    es6新增联系

--- src/main/webapp/static/src/js/react/main/list/list.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Administrator on 2017/6/29.
- */
-import List from '../../module/ul/list' ;
-ReactDOM.render(<List/>,document.getElementById('container'));
--- src/main/webapp/static/src/js/react/main/radio/radio.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Administrator on 2017/7/6.
- */
-import ChoiceQuestion from '../../module/radio/radio' ;
-ReactDOM.render(<ChoiceQuestion choices={['tom','jack']}/>,document.body);
